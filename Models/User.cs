eb9735fe23e0b5b7d4fb441675692bb1ae9a814b
culexgdubs <d4n13lb3rry@gmail.com>
2021-11-03 11:51:37 -0400

    added regex to user model

--- Models/User.cs
+++ Models/User.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.ComponentModel.DataAnnotations;
 
 namespace Models
 {
@@ -9,8 +11,15 @@ namespace Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9 !?']+$")]
         public string Username { get; set; }
 
         public List<FollowingPost> FollowingPosts {get; set;}
+
+        public override string ToString()
+        {
+            return $"Username: {this.Username} Following {FollowingPosts.Count} Posts";
+        }
     }
 }
